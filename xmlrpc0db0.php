1068cc9861eccea9ba346d77d5f684285265264b
amitkr54 <amitkr54@example.com>
2026-02-07 12:39:13 +0530

    SEO: Global localhost cleanup and sitemap optimization

--- xmlrpc0db0.php
+++ xmlrpc0db0.php
@@ -2,13 +2,13 @@
 	<service>
 		<engineName>WordPress</engineName>
 		<engineLink>https://wordpress.org/</engineLink>
-		<homePageLink>http://localhost/signageworks.in/</homePageLink>
+		<homePageLink>https://signageworks.in/</homePageLink>
 		<apis>
-			<api name="WordPress" blogID="1" preferred="true" apiLink="http://localhost/signageworks.in/xmlrpc.php" />
-			<api name="Movable Type" blogID="1" preferred="false" apiLink="http://localhost/signageworks.in/xmlrpc.php" />
-			<api name="MetaWeblog" blogID="1" preferred="false" apiLink="http://localhost/signageworks.in/xmlrpc.php" />
-			<api name="Blogger" blogID="1" preferred="false" apiLink="http://localhost/signageworks.in/xmlrpc.php" />
-				<api name="WP-API" blogID="1" preferred="false" apiLink="http://localhost/signageworks.in/wp-json/" />
+			<api name="WordPress" blogID="1" preferred="true" apiLink="https://signageworks.in/xmlrpc.php" />
+			<api name="Movable Type" blogID="1" preferred="false" apiLink="https://signageworks.in/xmlrpc.php" />
+			<api name="MetaWeblog" blogID="1" preferred="false" apiLink="https://signageworks.in/xmlrpc.php" />
+			<api name="Blogger" blogID="1" preferred="false" apiLink="https://signageworks.in/xmlrpc.php" />
+				<api name="WP-API" blogID="1" preferred="false" apiLink="https://signageworks.in/wp-json/" />
 			</apis>
 	</service>
 </rsd>
